ba5fc2b15bef8abc57457d973a030b27c5a1a62c
miloparra <miloparra1531.mp@gmail.com>
2025-04-04 14:46:30 +0200

    services - add service cards

--- pages/services.vue
+++ pages/services.vue
@@ -1,5 +1,96 @@
 <template>
-    <div>
-      <h1>Welcome to the services</h1>
+    <div class="flex justify-center items-center mt-10">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 justify-items-center">
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/lyon1.jpg"
+              alt="Visa" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Visa long séjour</h2>
+            <p>Demande de vise long séjour</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/eiffel1.jpg"
+              alt="Nat" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Nationalité</h2>
+            <p>Accés à la nationalité francaise</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/louvre1.jpg"
+              alt="Renew" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Renouvellement de titre de séjour</h2>
+            <p>Renouvellement de titre de séjour</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/brasserie1.jpg"
+              alt="Sejour" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Titre de séjour</h2>
+            <p>Première demande de titre de séjour</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/arc_triomphe1.jpg"
+              alt="Regularisation" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Régularisation</h2>
+            <p>Demande de régularisation</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+        <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+          <figure>
+            <img
+              class="h-70 object-cover"
+              src="../public/volcan1.jpg"
+              alt="Admin" />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">Démarches administratives</h2>
+            <p>Accompagnement pour les démarches administratives</p>
+            <div class="card-actions justify-end">
+              <button class="btn btn-primary">Voir plus</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
