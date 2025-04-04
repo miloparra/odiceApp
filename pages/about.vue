60e62a841e03486bf92031951f4e8915f12754df
miloparra <miloparra1531.mp@gmail.com>
2025-04-04 14:41:54 +0200

    about - add bg img, text and contact btn

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,18 @@
 <template>
-    <div>
-      <h1>Welcome to the about</h1>
+  <div class="relative w-full h-[2000px] top-[-65px]">
+    <img class="absolute w-full filter brightness-60" src="public/paris_nuit1.jpg" alt="">
+    <h1 class="absolute text-center mt-65 left-1/2 transform -translate-x-1/2 text-white text-6xl">
+      Aide à l'immigration en France
+    </h1>
+    <div class="absolute mt-120 left-1/2 transform -translate-x-1/2">
+        <button class="boutonContact btn btn-outline rounded-full px-10 p-6 text-lg text-white hover:bg-[oklch(45%_.24_277.023)] hover:border-[oklch(45%_.24_277.023)]">Nous contacter</button>
     </div>
+    <div class="absolute text-justify mt-200 left-1/2 transform -translate-x-1/2 bg-gray-50 p-15 text-xl">
+      <p>Je m’appelle Michel Droubay, et je suis le créateur d’ODice. Fort de plusieurs années d’expérience en droit des étrangers, j’ai accompagné des centaines de personnes dans leurs démarches administratives complexes, avec un focus particulier sur les demandes de titre de séjour, les procédures d’asile et les questions de régularisation.</p>
+      <br>
+      <p>Mon parcours m’a amené à travailler au sein d’associations solidaires et sur des projets humanitaires en France et à l’étranger. J’ai ainsi acquis une expertise pratique du système administratif français et une profonde compréhension des défis rencontrés par les étrangers dans leurs démarches.</p>
+      <br>
+      <p>ODice est né de ma volonté d’aider de manière simple, claire et accessible tous ceux qui se retrouvent face à la complexité administrative. Que vous soyez un particulier ou une entreprise, je vous accompagne dans la gestion de vos démarches administratives liées à la régularisation, au séjour, à l’asile, ou à la nationalité, en vous offrant des outils pratiques, des explications détaillées et une assistance personnalisée.</p>
+    </div>
+  </div>
 </template>
