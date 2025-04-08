0cee2de6a2e8eef476cf88c8359d8493a6cad3ff
miloparra <miloparra1531.mp@gmail.com>
2025-04-08 16:47:30 +0200

    footer - responsivity

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,16 @@
 <template>
-    <div class="flex justify-center items-center space-x-10 w-full h-40 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] z-50">
+    <div class="
+            flex flex-col md:flex-row 
+            justify-center items-center
+            space-y-2 md:space-x-10
+            w-full
+            h-40
+            shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]
+            z-50">
         <NuxtLink to="" class="hover:underline">Politique de confidentialité</NuxtLink>
-        <p>|</p>
+        <p class="hidden md:block">|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales d'utilisation (CGU)</NuxtLink>
-        <p>|</p>
+        <p class="hidden md:block">|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales de vente (CGV)</NuxtLink>
     </div>
 </template>
