6d5bf3c7e2edc956ae765eebebcf285c627581b4
miloparra <miloparra1531.mp@gmail.com>
2025-04-08 16:39:28 +0200

    footer - all vertical separators

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex justify-center items-center space-x-10 w-full h-40 shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)] z-50">
         <NuxtLink to="" class="hover:underline">Politique de confidentialité</NuxtLink>
+        <p>|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales d'utilisation (CGU)</NuxtLink>
+        <p>|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales de vente (CGV)</NuxtLink>
     </div>
 </template>
