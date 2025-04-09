1e420f8bac9048ae59f46b35a2c6acc081466b52
miloparra <miloparra1531.mp@gmail.com>
2025-04-09 16:24:00 +0200

    footer - update text size and responsivity

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="
-            flex flex-col md:flex-row 
+            flex flex-col lg:flex-row 
             justify-center items-center
-            space-y-2 md:space-x-10
+            text-sm lg:text-base
+            space-y-2 lg:space-x-10
             w-full
-            h-40
+            h-30
             shadow-[0_-4px_6px_-1px_rgba(0,0,0,0.1)]
             z-50">
         <NuxtLink to="" class="hover:underline">Politique de confidentialité</NuxtLink>
-        <p class="hidden md:block">|</p>
+        <p class="hidden lg:block">|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales d'utilisation (CGU)</NuxtLink>
-        <p class="hidden md:block">|</p>
+        <p class="hidden lg:block">|</p>
         <NuxtLink to="" class="hover:underline">Conditions générales de vente (CGV)</NuxtLink>
     </div>
 </template>
