8a524096f7c446d9cd2da572e4cf23ef5553133c
miloparra <miloparra1531.mp@gmail.com>
2025-04-09 16:14:51 +0200

    services - remove buttons and update descriptions

--- pages/services.vue
+++ pages/services.vue
@@ -7,10 +7,10 @@
         </figure>
         <div class="card-body">
           <h2 class="card-title">Visa long séjour</h2>
-          <p>Demande de vise long séjour</p>
-          <div class="card-actions justify-end">
+          <p>Demande de visa long séjour vers la France : vie privée et familiale, visiteur, carte RECE, etc.</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
@@ -18,11 +18,11 @@
           <img class="h-70 scale-150 object-cover" src="../public/pictogram/2.png" alt="Nat" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Nationalité</h2>
-          <p>Accés à la nationalité francaise</p>
-          <div class="card-actions justify-end">
+          <h2 class="card-title">Accès à la nationalité</h2>
+          <p>Accompagnement à l'accès à la nationalité française</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
@@ -30,11 +30,11 @@
           <img class="h-70 scale-150 object-cover" src="../public/pictogram/3.png" alt="Renew" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Renouvellement de titre de séjour</h2>
-          <p>Renouvellement de titre de séjour</p>
-          <div class="card-actions justify-end">
+          <h2 class="card-title">Titre de séjour</h2>
+          <p>Première demande et renouvellement de titre de séjour</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
@@ -42,11 +42,11 @@
           <img class="h-70 scale-150 object-cover" src="../public/pictogram/4.png" alt="Sejour" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Titre de séjour</h2>
-          <p>Première demande de titre de séjour</p>
-          <div class="card-actions justify-end">
+          <h2 class="card-title">Carte de résident</h2>
+          <p>Accompagnement pour la demande de carte de résident</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
@@ -54,11 +54,11 @@
           <img class="h-70 scale-150 object-cover" src="../public/pictogram/5.png" alt="Regularisation" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Régularisation</h2>
-          <p>Demande de régularisation</p>
-          <div class="card-actions justify-end">
+          <h2 class="card-title">Demande de régularisation</h2>
+          <p>Procédure de régularisation en France (par le travail ou pour motif familial)</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
       <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
@@ -67,10 +67,10 @@
         </figure>
         <div class="card-body">
           <h2 class="card-title">Démarches administratives</h2>
-          <p>Accompagnement pour les démarches administratives</p>
-          <div class="card-actions justify-end">
+          <p>Accompagnement et formation sur les démarches administratives</p>
+          <!-- <div class="card-actions justify-end">
             <button class="btn btn-primary">Voir plus</button>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
