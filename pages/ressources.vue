1659247c6e4516eb7ac411936f1e4b0202af968e
miloparra <miloparra1531.mp@gmail.com>
2025-04-10 23:48:13 +0200

    ressources - add downloadable ressources

--- pages/ressources.vue
+++ pages/ressources.vue
@@ -1,5 +1,90 @@
 <template>
-  <div style="height: calc(100vh - 185px)" class="flex justify-center items-center">
-    <h1 class="text-2xl">Welcome to the ressources</h1>
+  <div class="flex justify-center items-center mt-10 mb-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 justify-items-center">
+      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+        <figure>
+          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/1.png" alt="Visa" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Guide du demandeur d'asile</h2>
+          <p>Guide pour les demandeurs d'asile en france</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" download>
+                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
+              </a>
+            </button>
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+        <figure>
+          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/2.png" alt="Visa" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Guide régularisation</h2>
+          <p>Guide pour réussir son entretient de naturalisation française</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" download>
+                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
+              </a>
+            </button>
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+        <figure>
+          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/3.png" alt="Visa" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">50 Questions</h2>
+          <p>50 questions pour réussir son entretient de naturalisation française</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" download>
+                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
+              </a>
+            </button>
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+        <figure>
+          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/4.png" alt="Visa" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Guide administratif</h2>
+          <p>Guide pour être autonome administrativement</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" download>
+                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
+              </a>
+            </button>
+            <button class="btn btn-primary">
+              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
+                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
