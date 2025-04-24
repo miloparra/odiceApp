cc53222e32e4136e07ebb7abf9dcbe7bd3f533fb
miloparra <miloparra1531.mp@gmail.com>
2025-04-24 15:47:10 +0200

    services - contentful configuration

--- pages/services.vue
+++ pages/services.vue
@@ -1,76 +1,28 @@
+<script setup>
+import { client } from '@/lib/contentful'
+
+const services = ref([])
+
+onMounted(async () => {
+  const res = await client.getEntries({
+    content_type: 'services'
+  })
+  services.value = res.items
+})
+</script>
+
 <template>
   <div class="flex justify-center items-center mt-10 mb-10">
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 justify-items-center">
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+      <div v-for="service in services" :key="service.sys.id" class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
         <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/1.png" alt="Visa" />
+          <img class="h-70 scale-150 object-cover"
+          :src="`https:${service.fields.pictogram.fields.file.url}`"
+          :alt="service.fields.pictogram.fields.title || 'Service image'" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Visa long séjour</h2>
-          <p>Demande de visa long séjour vers la France : vie privée et familiale, visiteur, carte RECE, etc.</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/2.png" alt="Nat" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Accès à la nationalité</h2>
-          <p>Accompagnement à l'accès à la nationalité française</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/3.png" alt="Renew" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Titre de séjour</h2>
-          <p>Première demande et renouvellement de titre de séjour</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/4.png" alt="Sejour" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Carte de résident</h2>
-          <p>Accompagnement pour la demande de carte de résident</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/5.png" alt="Regularisation" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Demande de régularisation</h2>
-          <p>Procédure de régularisation en France (par le travail ou pour motif familial)</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-70 scale-150 object-cover" src="../public/pictogram/6.png" alt="Admin" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Démarches administratives</h2>
-          <p>Accompagnement et formation sur les démarches administratives</p>
-          <!-- <div class="card-actions justify-end">
-            <button class="btn btn-primary">Voir plus</button>
-          </div> -->
+          <h2 class="card-title">{{ service.fields.title }}</h2>
+          <p>{{ service.fields.description }}</p>
         </div>
       </div>
     </div>
