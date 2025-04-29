c4f158c4e9c655aaad99dd743a7d21b0236498d9
miloparra <miloparra1531.mp@gmail.com>
2025-04-29 13:31:01 +0200

    ressources - contentful configuration

--- pages/ressources.vue
+++ pages/ressources.vue
@@ -1,84 +1,36 @@
+<script setup>
+import { client } from '@/lib/contentful'
+
+const ressources = ref([])
+
+onMounted(async () => {
+  const res = await client.getEntries({
+    content_type: 'ressources'
+  })
+  ressources.value = res.items
+})
+</script>
+
 <template>
   <div class="flex justify-center items-center mt-10 mb-10">
     <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-3 justify-items-center">
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
+      <div v-for="ressource in ressources" :key="ressource.sys.id" class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
         <figure>
-          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/1.png" alt="Visa" />
+          <img class="h-40 scale-150 object-cover"
+          :src="`https:${ressource.fields.pictogram.fields.file.url}`"
+          :alt="ressource.fields.pictogram.fields.title || 'Ressource image'" />
         </figure>
         <div class="card-body">
-          <h2 class="card-title">Guide du demandeur d'asile</h2>
-          <p>Guide pour les demandeurs d'asile en france</p>
+          <h2 class="card-title">{{ ressource.fields.title }}</h2>
+          <p>{{ ressource.fields.description }}</p>
           <div class="card-actions justify-end">
             <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" download>
+              <a :href="`https:${ressource.fields.document.fields.file.url}`" download>
                 <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
               </a>
             </button>
             <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
-                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/2.png" alt="Visa" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Guide régularisation</h2>
-          <p>Guide pour réussir son entretient de naturalisation française</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" download>
-                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
-              </a>
-            </button>
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
-                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/3.png" alt="Visa" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">50 Questions</h2>
-          <p>50 questions pour réussir son entretient de naturalisation française</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" download>
-                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
-              </a>
-            </button>
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
-                <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
-              </a>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="card bg-base-100 w-9/10 lg:w-96 shadow-sm">
-        <figure>
-          <img class="h-40 scale-150 object-cover" src="../public/pictogramR/4.png" alt="Visa" />
-        </figure>
-        <div class="card-body">
-          <h2 class="card-title">Guide administratif</h2>
-          <p>Guide pour être autonome administrativement</p>
-          <div class="card-actions justify-end">
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" download>
-                <font-awesome-icon icon="fa-solid fa-download" class="text-base mt-1" />
-              </a>
-            </button>
-            <button class="btn btn-primary">
-              <a href="/docs/guide_naturalisation.pdf" target="_blank" rel="noopener noreferrer">
+              <a :href="`https:${ressource.fields.document.fields.file.url}`" target="_blank" rel="noopener noreferrer">
                 <font-awesome-icon icon="fa-solid fa-eye" class="text-base mt-1" />
               </a>
             </button>
