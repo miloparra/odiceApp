23b62d49cf2c846718684efd8e146f50834d8921
miloparra <miloparra1531.mp@gmail.com>
2025-09-26 11:30:36 +0200

    about - change the about section bg and add services link button

--- pages/about.vue
+++ pages/about.vue
@@ -50,63 +50,89 @@ watch(lang, (newLang) => {
 const contactLabel = computed(() =>
   lang.value === 'fr' ? 'Nous contacter' : 'Contact us'
 )
+
+const serviceLabel = computed(() =>
+  lang.value === 'fr' ? 'Nos services' : 'Our services'
+)
 </script>
 
 <template>
-  <section id="home">
-    <img style="height: calc(100vh + 65px)" class="absolute w-full top-[-65px] filter brightness-80 object-cover"
-    src="public/volcanIA2.png" alt="">
-    <div class="reactive w-full h-screen top-[-65px]">
-      <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-10">
-        <h1 class="
-            text-center
-            welcomeTitle
-            text-white
-            text-4xl
-            lg:text-6xl">
-          {{ about?.title }}
-        </h1>
-        <NuxtLink to="/contact" class="flex justify-center">
-          <button class="
-              boutonContact btn
-              btn-outline
-              rounded-full
-              p-3 px-5
-              lg:p-6 lg:px-10
-              text-sm
-              lg:text-lg
+  <div class="bg-gray-50">
+    <section id="home">
+      <img style="height: calc(100vh + 65px)" class="absolute w-full top-[-65px] filter brightness-80 object-cover"
+      src="public/volcanIA2.png" alt="">
+      <div class="reactive w-full h-screen top-[-65px]">
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-10">
+          <h1 class="
+              text-center
+              welcomeTitle
               text-white
-              hover:bg-[oklch(45%_.24_277.023)]
-              hover:border-[oklch(45%_.24_277.023)]">
-              {{ contactLabel }}
-          </button>
-        </NuxtLink>
+              text-4xl
+              lg:text-6xl">
+            {{ about?.title }}
+          </h1>
+          <div class="flex justify-center gap-5">
+            <NuxtLink to="/contact" class="flex justify-center">
+              <button class="
+                  rounded-full
+                  px-10
+                  py-3
+                  text-sm
+                  lg:text-lg
+                  font-semibold
+                  text-white
+                  border-1
+                  hover:bg-[oklch(45%_.24_277.023)]
+                  hover:border-[oklch(45%_.24_277.023)]
+                  cursor-pointer">
+                  {{ contactLabel }}
+              </button>
+            </NuxtLink>
+            <NuxtLink to="/services" class="flex justify-center">
+              <button class="
+                  rounded-full
+                  px-10
+                  py-3
+                  text-sm
+                  lg:text-lg
+                  font-semibold
+                  text-white
+                  border-1
+                  hover:text-black
+                  hover:bg-[#8FDA59]
+                  hover:border-[#8FDA59]
+                  cursor-pointer">
+                  {{ serviceLabel }}
+              </button>
+            </NuxtLink>
+          </div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
 
-  <section id="about" class="scroll-mt-[65px]">
-    <div class="flex justify-center mt-[-65px]">
-      <div class="
-          relative
-          text-justify
-          w-5/6
-          mt-20 mb-20
-          bg-gray-100
-          p-7 lg:p-15
-          text-base
-          lg:text-lg">
-        <p class="guillemets absolute top-[-18px] lg:top-[-25px] left-[2px] text-8xl lg:text-9xl text-zinc-600">"</p>
-        <img class="
-          float-left
-          w-70
-          h-70
-          object-cover
-          p-7 pb-11 lg:pr-7 lg:pt-0 lg:pb-7 lg:pl-0" src="../public/michel.jpeg" />
-        <p class="whitespace-pre-line">{{ about?.presentation }}</p>
-        <p class="guillemets absolute right-[5px] bottom-[-75px] lg:bottom-[-100px] text-8xl lg:text-9xl text-zinc-600">"
-        </p>
+    <section id="about" class="scroll-mt-[65px]">
+      <div class="flex justify-center mt-[-65px]">
+        <div class="
+            relative
+            text-justify
+            w-5/6
+            mt-20 mb-20
+            bg-white
+            p-7 lg:p-15
+            text-base
+            lg:text-lg">
+          <p class="guillemets absolute top-[-18px] lg:top-[-25px] left-[2px] text-8xl lg:text-9xl text-zinc-600">"</p>
+          <img class="
+            float-left
+            w-70
+            h-70
+            object-cover
+            p-7 pb-11 lg:pr-7 lg:pt-0 lg:pb-7 lg:pl-0" src="../public/michel.jpeg" />
+          <p class="whitespace-pre-line">{{ about?.presentation }}</p>
+          <p class="guillemets absolute right-[5px] bottom-[-75px] lg:bottom-[-100px] text-8xl lg:text-9xl text-zinc-600">"
+          </p>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
